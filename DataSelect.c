0948ae6a8f2c3edda3a53152ac0ba558c3565a79
davidsog <89423145+davidsog@users.noreply.github.com>
2021-10-22 14:30:03 +0300

    Update DataSelect.c

--- DataSelect.c
+++ DataSelect.c
@@ -1,66 +1,79 @@
 #include <stdio.h>
 #include <math.h>
 
-int DataSelect(int M, float R[]);
+int DataSelect(int Num, float R[]);
 void MasOut (float *a, int Size);
 
-int main()
-{
-	float a[] = {10000, 9999, 10001, 10001, 9999, 9998, 10002, 10000};
-	int K = 8;
-	
-	K = DataSelect(K, a);
-	printf("K=%i\n", K);
-	MasOut(a, K);	
-
-	return 0;
-}
+/*int main()
+{       
+        float a[] = {10000, 9999, 10001, 10001, 9999, 9998, 10002, 10000};
+        int K = 8;
+        
+        K = DataSelect(K, a);
+        printf("K=%i\n", K);
+        MasOut(a, K);
+        
+        return 0;
+}*/
 
 //R - resistance
-float Averange(int M, float R[])
-{
-	float Sum = 0;
-	int i = 0;
-		
-	for (i = 0; i < M; i++)
-		Sum += R[i];
+float Averange(int Num, float R[])
+{       
+        if(R == NULL)
+                return 0;
 
-	printf("Averange = %f\n", Sum/M);
-	return Sum/M;
+        float Sum = 0;
+        int i = 0;
+
+        for (i = 0; i < Num; i++)
+                Sum += R[i];
+
+        printf("Averange = %f\n", Sum/Num);
+        return Sum/Num;
 }
 
-void DeletEl(float R[], int M)
+void DeletEl(float R[], int Num)
 {
-	int i = 0;
+        if(R == NULL)
+                return 0;
 
-	for (i = 1; i < M; i++)
-		R[i - 1] = R[i];
+        int i = 0;
 
-	R[M - 1] = 0;
+        for (i = 1; i < Num; i++)
+                R[i - 1] = R[i];
+
+        R[Num - 1] = 0;
 }
 
-int DataSelect(int M, float R[])
+
+int DataSelect(int Num, float R[])
 {
-	int i = 0, K = 0;
-	float averange = Averange(M, R);
+        if(R == NULL)
+                return 0;
 
-	for (i = 0; i < M; i++)
-		if(fabs((R[i] - averange)/averange) > 0.03)
-		{	
-			printf("d = %f\n",fabs((R[i] - averange)/averange));
+        int i = 0, K = 0;
+        float averange = Averange(Num, R);
 
-			DeletEl(&(R[i]), M - i - 1);
-			K++;
-		}	
+        for (i = 0; i < Num; i++)
+                if(fabs((R[i] - averange)/averange) > 0.03)
+                {
+                        printf("d = %f\n",fabs((R[i] - averange)/averange));
 
-	return M - K;
+                        DeletEl(&(R[i]), Num - i - 1);
+                        K++;
+                }
+
+        return Num - K;
 }
 
 void MasOut (float *a, int Size)
 {
-	int i = 0; 
+        if(R == NULL)
+                return 0;
 
-	for(i = 0; i < Size; i++)
-		printf("%.1lf  ", a[i]);
-	printf("\n");
+        int i = 0;
+
+        for(i = 0; i < Size; i++)
+                printf("%.1lf  ", a[i]);
+        printf("\n");
 }
