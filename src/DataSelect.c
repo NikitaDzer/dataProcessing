2798ead5b6754dfb56cbab5a2e6ded19a6a18e8c
davidsog <89423145+davidsog@users.noreply.github.com>
2021-10-22 16:29:09 +0300

    Add files via upload

---
+++ src/DataSelect.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <math.h>
+
+int DataSelect(int M, float R[]);
+void MasOut (float *a, int Size);
+
+int main()
+{
+	float a[] = {0.111782, 0.111622, 0.112505, 0.114052,
+			  0.11431, 0.110154, 0.098418, 0.111481};
+	int K = 8;
+	
+	K = DataSelect(K, a);
+	printf("K=%i\n", K);
+	MasOut(a, K);	
+
+	return 0;
+}
+
+//R - resistance
+float Averange(int M, float R[])
+{
+	if(R == NULL)
+		return 0;
+	
+	float Sum = 0;
+	int i = 0;
+		
+	for (i = 0; i < M; i++)
+		Sum += R[i];
+
+	printf("Averange = %f\n", Sum/M);
+	return Sum/M;
+}
+
+void DeletEl(float R[], int M)
+{
+	if(R == NULL)
+		return;
+
+	printf("DeletMas\n");	
+	MasOut(R, M);
+	int i = 0;
+
+	for (i = 1; i <= M; i++)
+		R[i - 1] = R[i];
+
+	R[M - 1] = 0;
+}
+
+int DataSelect(int M, float R[])
+{
+	if(R == NULL)
+		return 0;
+	
+	int i = 0;
+	float averange = Averange(M, R), a = 0;
+
+	//MasOut(R, M);
+	for (i = 0; i < M; i++)
+		if((a = fabs((R[i] - averange)/averange)) > 0.03)
+		{	
+			printf("d = %f, ",a);
+			printf("R[i] = %f\n", R[i]);
+			MasOut(R, M);
+			DeletEl(&(R[i]), M - i);
+			M--;
+		}	
+	
+	return M;
+}
+
+void MasOut (float *a, int Size)
+{
+	if(a == NULL)
+		return;
+	
+	int i = 0; 
+
+	for(i = 0; i < Size; i++)
+		printf("%.5lf  ", a[i]);
+	printf("\n");
+}
